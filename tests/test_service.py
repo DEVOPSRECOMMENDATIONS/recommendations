2f8efc5a13040d04fbb63732d838d5543e8babc2
sa6219 <sa6219@stern.nyu.edu>
2021-04-19 17:41:24 -0400

    List methods ia updated with search by Recommendation  Type, Product A, and Product B

--- tests/test_service.py
+++ tests/test_service.py
@@ -43,6 +43,21 @@ class TestRecommendationServer(TestCase):
         db.session.remove()
         db.drop_all()
 
+    def _create_recommendation_array(self, count):
+        recommendations = []
+        for _ in range(count):
+            test_recommendation = RecommendationFactory()
+            resp = self.app.post(
+                '/recommendations', json=test_recommendation.serialize(), content_type='application/json'
+            )
+            self.assertEqual(
+                resp.status_code, status.HTTP_201_CREATED, "Could not create test recommendation"
+            )
+            new_recommendation= resp.get_json()
+            test_recommendation.id = new_recommendation["id"]
+            recommendations.append(test_recommendation)
+        return recommendations
+
     def _create_recommendation(self):
         return Recommendation(
             product_a="ProductA", 
@@ -50,7 +65,6 @@ class TestRecommendationServer(TestCase):
             recom_type="U", 
             likes=0
         )
-
 ######################################################################
 #  P L A C E   T E S T   C A S E S   H E R E 
 ######################################################################
@@ -175,7 +189,52 @@ class TestRecommendationServer(TestCase):
         self.assertEqual(likes_count["likes"], 1)
 
 
+    def test_query_by_product_A(self):
+        """ Query Recommendations by Product A """
+        test_recommendation = self._create_recommendation()
+        test_recommendation.create()
+        resp = self.app.get(
+            "/recommendations/{}".format(test_recommendation.id), content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        self.assertTrue(len(resp.data) > 0)
+        self.assertIn(b'ProductA', resp.data)
+        self.assertNotIn(b'hats', resp.data)
+        data = resp.get_json()
+        logging.debug('data = %s', data)
+        self.assertEqual(data["product_a"], test_recommendation.product_a)
 
+
+    def test_query_by_product_B(self):
+        """ Query Recommendations by Product B """
+        test_recommendation = self._create_recommendation()
+        test_recommendation.create()
+        resp = self.app.get(
+            "/recommendations/{}".format(test_recommendation.id), content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        self.assertTrue(len(resp.data) > 0)
+        self.assertIn(b'ProductB', resp.data)
+        self.assertNotIn(b'hats', resp.data)
+        data = resp.get_json()
+        logging.debug('data = %s', data)
+        self.assertEqual(data["product_b"], test_recommendation.product_b)
+
+
+    def test_query_by_Recom_Type(self):
+        """ Query Recommendations by Recommendation Type """
+        test_recommendation = self._create_recommendation()
+        test_recommendation.create()
+        resp = self.app.get(
+            "/recommendations/{}".format(test_recommendation.id), content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        self.assertTrue(len(resp.data) > 0)
+        self.assertIn(b'recom_type', resp.data)
+        self.assertNotIn(b'hats', resp.data)
+        data = resp.get_json()
+        logging.debug('data = %s', data)
+        self.assertEqual(data["recom_type"], test_recommendation.recom_type)
 
 
     def test_get_recommendation_not_found(self):
@@ -202,3 +261,45 @@ class TestRecommendationServer(TestCase):
                              json=test_recommendation.serialize(),
                              content_type="bad content")
         self.assertEqual(resp.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+
+    def test_query_recommendation_list_by_recomm_Type(self):
+        """ Query Recommendations by Category """
+        recommendations = self._create_recommendation_array(10)
+        test_recom_type = recommendations[0].recom_type
+        category_recommendations = [recommendations for recommendation in recommendations if recommendation.recom_type == test_recom_type]
+        resp = self.app.get('/recommendations', query_string="recom_type={}".format(test_recom_type))
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data), len(category_recommendations))
+        # check the data just to be sure
+        for recommendation in data:
+            self.assertEqual(recommendation["recom_type"], test_recom_type)
+    
+    def test_query_recommendation_list_by_Product_A(self):
+        """ Query Recommendations by Product A """
+        recommendations = self._create_recommendation_array(10)
+        test_product_a = recommendations[0].recom_type
+        category_recommendations = [recommendations for recommendation in recommendations if recommendation.product_a == test_product_a]
+        resp = self.app.get('/recommendations', query_string="product_a={}".format(test_product_a))
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data), len(category_recommendations))
+        # check the data just to be sure
+        for recommendation in data:
+            self.assertEqual(recommendation["product_a"], test_product_a)
+
+    def test_query_recommendation_list_by_Product_B(self):
+        """ Query Recommendations by Product B """
+        recommendations = self._create_recommendation_array(10)
+        test_product_b = recommendations[0].recom_type
+        category_recommendations = [recommendations for recommendation in recommendations if recommendation.product_b == test_product_b]
+        resp = self.app.get('/recommendations', query_string="product_b={}".format(test_product_b))
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data), len(category_recommendations))
+        # check the data just to be sure
+        for recommendation in data:
+            self.assertEqual(recommendation["product_b"], test_product_b)
+            
+            
+
